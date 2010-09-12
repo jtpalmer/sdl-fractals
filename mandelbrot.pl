726d5ce5602f88ce19730dfe56af1b2f80531db9
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2010-09-12 17:14:54 -0400

    Perltidy.

--- mandelbrot.pl
+++ mandelbrot.pl
@@ -9,15 +9,15 @@ use Math::Fractal::Mandelbrot;
 my $title  = 'Mandelbrot Fractal';
 my $width  = 640;
 my $height = 480;
-my $view   = [ -2, -1, 1, 1 ]; # [ min_x, $min_y, $max_x, $max_y]
+my $view   = [ -2, -1, 1, 1 ];       # [ min_x, $min_y, $max_x, $max_y]
 
 Math::Fractal::Mandelbrot->set_max_iter(255);
-Math::Fractal::Mandelbrot->set_bounds(@$view, $width, $height);
+Math::Fractal::Mandelbrot->set_bounds( @$view, $width, $height );
 
 my $app = SDLx::App->new(
-	title  => $title,
-	width  => $width,
-	height => $height,
+    title  => $title,
+    width  => $width,
+    height => $height,
 );
 
 my $format = $app->surface->format;
@@ -26,20 +26,21 @@ my @palette
 $palette[0] = SDL::Video::map_RGB( $format, 0, 0, 0 );
 
 foreach my $y ( 0 .. $height - 1 ) {
-	foreach my $x ( 0 .. $width - 1 ) {
-        $app->[$x][$y] = $palette[Math::Fractal::Mandelbrot->point($x, $y)];
-	}
+    foreach my $x ( 0 .. $width - 1 ) {
+        $app->[$x][$y]
+            = $palette[ Math::Fractal::Mandelbrot->point( $x, $y ) ];
+    }
     $app->update;
 }
 $app->update;
 
 $app->add_event_handler(
-	sub {
-		my ($event) = @_;
-		return 0 if $event->type == SDL_QUIT;
-		return 0 if $event->key_sym == SDLK_ESCAPE;
+    sub {
+        my ($event) = @_;
+        return 0 if $event->type == SDL_QUIT;
+        return 0 if $event->key_sym == SDLK_ESCAPE;
         return 1;
-	}
+    }
 );
 
 $app->run;
